3a8c0beae644b664c2f8c8793c0f5ba9bd98a42a
Fabio Bulgarella <fabio.bulgarella@gmail.com>
2018-04-18 01:29:52 +0200

    Implemented AsyncTask to get book details in a separate thread without blocking UI; not well tested due to firebase problem (E/FirebaseInstanceId: Token retrieval failed: SERVICE_NOT_AVAILABLE).

--- app/src/main/java/it/polito/mad/sharenbook/ShareBookActivity.java
+++ app/src/main/java/it/polito/mad/sharenbook/ShareBookActivity.java
@@ -1,6 +1,8 @@
 package it.polito.mad.sharenbook;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +10,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,15 +19,22 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import it.polito.mad.sharenbook.Utils.InputValidate;
+
 public class ShareBookActivity extends AppCompatActivity {
 
+    // View objects
     private EditText editIsbn;
     private Button btnSearchIsbn;
     private CardView btnScan;
     private CardView btnManual;
     private BottomNavigationView navBar;
 
+    // Barcode scanner object
     private IntentIntegrator qrScan;
+
+    // ProgressDialog
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -46,7 +56,10 @@ public class ShareBookActivity extends AppCompatActivity {
         // Setup navbar
         setupNavbar();
 
-        // View objects
+        // Setup progress dialog
+        progressDialog = new ProgressDialog(ShareBookActivity.this, ProgressDialog.STYLE_SPINNER);
+
+        // Get view objects
         editIsbn = findViewById(R.id.sba_edit_text_isbn);
         btnSearchIsbn = findViewById(R.id.sba_search_isbn_button);
         btnScan = findViewById(R.id.sba_scan_button);
@@ -61,22 +74,15 @@ public class ShareBookActivity extends AppCompatActivity {
         btnSearchIsbn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String insertedText = editIsbn.getText().toString();
-
-                if (insertedText.length() != 10 && insertedText.length() != 13) {
-                    editIsbn.setError(getText(R.string.sba_isbn_not_valid));
+                // Validate isbn value
+                if (InputValidate.wrongIsbn(editIsbn)) {
+                    editIsbn.setError(getText(R.string.isbn_bad_format));
                     return;
                 }
 
-                BookDetails bookInfo = new BookDetails(insertedText);
-                if (bookInfo.getTotalItems() == 0) {
-                    Toast.makeText(getApplicationContext(), "No books found", Toast.LENGTH_SHORT).show();
-                }
-                else {
-                    Intent i = new Intent(getApplicationContext(), EditBookActivity.class);
-                    i.putExtra("book", bookInfo.getBookList().get(0));
-                    startActivity(i);
-                }
+                // Retrieve book details on a separate thread
+                String insertedIsbn = editIsbn.getText().toString();
+                new GetBookDetailsTask().execute(insertedIsbn);
             }
         });
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -104,24 +110,12 @@ public class ShareBookActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        BookDetails bookInfo;
 
         if(result != null)
         {
-            if(result.getContents() == null) return;
-            else
-            {
-                bookInfo = new BookDetails(result.getContents());
-
-                if (bookInfo.getTotalItems() > 0)
-                {
-                    // Fire EditBookActivity
-                    Intent i = new Intent(getApplicationContext(), EditBookActivity.class);
-                    i.putExtra("book", bookInfo.getBookList().get(0));
-                    startActivity(i);
-                }
-                else
-                    Toast.makeText(getApplicationContext(), "Invalid ISBN", Toast.LENGTH_SHORT).show();
+            if (result.getContents() != null) {
+                // Retrieve book details on a separate thread
+                new GetBookDetailsTask().execute(result.getContents());
             }
         }
         else
@@ -134,6 +128,7 @@ public class ShareBookActivity extends AppCompatActivity {
     @Override
     public boolean onSupportNavigateUp()
     {
+        // Terminate activity (actionbar left arrow pressed)
         finish();
         return true;
     }
@@ -149,11 +144,12 @@ public class ShareBookActivity extends AppCompatActivity {
     {
         navBar = findViewById(R.id.navigation);
 
-        //set navigation_profile as selected item
+        // Set navigation_shareBook as selected item
         navBar.setSelectedItemId(R.id.navigation_shareBook);
 
-        //set the listener for the navigation bar items
+        // Set the listeners for the navigation bar items
         navBar.setOnNavigationItemSelectedListener(item -> {
+
             switch (item.getItemId()) {
                 case R.id.navigation_showcase:
                     Toast.makeText(getApplicationContext(), "Selected Showcase!", Toast.LENGTH_SHORT).show();
@@ -169,7 +165,51 @@ public class ShareBookActivity extends AppCompatActivity {
                 case R.id.navigation_shareBook:
                     break;
             }
+
             return true;
         });
+    }
+
+    private class GetBookDetailsTask extends AsyncTask<String, Integer, BookDetails> {
+
+        // Runs in UI before background thread is called
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Do something like display a progress bar
+            progressDialog.setMessage(getText(R.string.sba_getting_book_details));
+            progressDialog.show();
+        }
+
+        // This is run in a background thread
+        @Override
+        protected BookDetails doInBackground(String... params) {
+            // Get the isbn string from params, which is an array
+            String readIsbn = params[0];
+
+            for (int i=0; i < 100000; i++)
+                Log.d("Testing ProgressDialog:", "iteration n° " + i);
+
+            return new BookDetails(readIsbn);
+        }
+
+        // This runs in UI when background thread finishes
+        @Override
+        protected void onPostExecute(BookDetails result) {
+            super.onPostExecute(result);
+
+            // Do things like hide the progress bar or change a TextView
+            progressDialog.dismiss();
+
+            if (result.getTotalItems() == 0) {
+                Toast.makeText(getApplicationContext(), R.string.sba_no_books_found, Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent i = new Intent(getApplicationContext(), EditBookActivity.class);
+                i.putExtra("book", result.getBookList().get(0));
+                startActivity(i);
+            }
+        }
     }
 }
---
+++ app/src/main/java/it/polito/mad/sharenbook/Utils/InputValidate.java
@@ -0,0 +1,17 @@
+package it.polito.mad.sharenbook.Utils;
+
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
+
+public class InputValidate {
+
+    //Regex for input validation
+    private static final Pattern ISBN_REGEX = Pattern.compile("^(97(8|9))?\\d{9}(\\d|X)$");
+
+    // Validate ISBN (10 or 13 digits, in latter case it should start with 978 or 979)
+    public static boolean wrongIsbn(EditText input) {
+        String data = input.getText().toString();
+        return !ISBN_REGEX.matcher(data).matches();
+    }
+}
