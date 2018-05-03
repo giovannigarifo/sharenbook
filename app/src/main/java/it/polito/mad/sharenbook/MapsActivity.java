41109684337e01eac253f0ee3c24e92d3b5e1748
Fabio Bulgarella <fabio.bulgarella@gmail.com>
2018-05-03 22:40:11 +0200

    Some bugs solved;

--- app/src/main/java/it/polito/mad/sharenbook/MapsActivity.java
+++ app/src/main/java/it/polito/mad/sharenbook/MapsActivity.java
@@ -64,6 +64,9 @@ public class MapsActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        // RecylerView setup
+        searchResult = new ArrayList<>();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
